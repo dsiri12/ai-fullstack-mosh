7cde482725a9366fab0b07580fdf4b5da650a42a
Damien Siri <dsiri12@gmail.com>
2026-07-15 14:47:10 -0400

    setup prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,14 +8,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('hi there');
-})
+   res.send('hi there');
+});
 
 app.get('/api/v1/hello', (req: Request, res: Response) => {
-  res.send({message: 'Hello from the server!'});
-})
+   res.send({ message: 'Hello from the server!' });
+});
 
-// js arrow function === lambda function in other languages 
+// js arrow function === lambda function in other languages
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-})
+   console.log(`Server is running on http://localhost:${port}`);
+});
