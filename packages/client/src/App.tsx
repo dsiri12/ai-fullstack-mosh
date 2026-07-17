1a8564ae5e03a5555a033c8d002685e472343a7c
Damien Siri <dsiri12@gmail.com>
2026-07-17 19:48:16 -0400

    let AI answer questions related to our chatbot setup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,10 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { Button } from './components/ui/button';
+import ChatBot from './components/chat/ChatBot';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('/api/v1/hello')
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   }, []);
-
    return (
-      <div className="App">
-         <h1 className="font-bold p-4 text-3xl">{message}</h1>
-         <Button>Click me!</Button>
+      <div className="p-4 h-screen w-full">
+         <ChatBot />
       </div>
    );
 }
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,19 +1,17 @@
 import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import router from './routes';
+import { randomUUID } from 'crypto';
+const id = randomUUID();
+console.log(id);
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-   res.send('hi there');
-});
-
-app.get('/api/v1/hello', (req: Request, res: Response) => {
-   res.send({ message: 'Hello from the server!' });
-});
 
 // js arrow function === lambda function in other languages
 app.listen(port, () => {
